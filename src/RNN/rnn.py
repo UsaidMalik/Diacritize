59cca9380f7bb601f344c5edfd9608a16a2a0283
szcharlesji <szcharlesji@gmail.com>
2024-04-10 01:19:12 -0400

    changed features and model to single layer

--- src/RNN/rnn.py
+++ src/RNN/rnn.py
@@ -1,7 +1,6 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Embedding
-from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 
 training_file_path = './data/Parsed Data/quran-letter.txt'
@@ -14,33 +13,39 @@ with open(training_file_path, 'r', encoding='utf-8') as f:
 data = [line.split('\t') for line in lines if line]
 
 # Create mappings
-char_to_int = {char: i for i, char in enumerate(sorted(set(char for pair in data for char in pair)))}
-int_to_char = {i: char for char, i in char_to_int.items()}
-
-# Convert data to sequences of integers
-X = [[char_to_int[char] for char in pair] for pair in data]
+consonants = sorted(set(pair[0] for pair in data))
+vowels = sorted(set(pair[1] for pair in data))
+cons_to_int = {char: i for i, char in enumerate(consonants)}
+vowel_to_int = {mark: i for i, mark in enumerate(vowels)}
+int_to_cons = {i: char for char, i in cons_to_int.items()}
+int_to_vowel = {i: mark for mark, i in vowel_to_int.items()}
 
 # Separate letters and marks
-letters = [pair[0] for pair in X]
-marks = [pair[1] for pair in X]
+consonants = [cons_to_int[pair[0]] for pair in data]
+vowels = [vowel_to_int[pair[1]] for pair in data]
 
-# Convert to one-hot encoding
-letters = to_categorical(letters, num_classes=len(char_to_int))
-marks = to_categorical(marks, num_classes=len(char_to_int))
+# Convert letters to one-hot encoding
+num_classes = len(cons_to_int)
+consonants = to_categorical(consonants, num_classes=num_classes)
 
 # Define your model
 model = Sequential()
-model.add(Embedding(input_dim=len(char_to_int)+1, output_dim=64, input_length=letters.shape[1]))
-model.add(LSTM(64, return_sequences=False))
-model.add(Dense(len(char_to_int), activation='softmax'))
+model.add(LSTM(64, input_shape=(1, num_classes)))
+model.add(Dense(len(vowel_to_int), activation='softmax'))
 
 # Compile and train your model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(letters, marks, epochs=1, batch_size=64)
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(consonants.reshape((-1, 1, num_classes)), np.array(vowels), epochs=1, batch_size=64)
 
 # Predict marks for new text
 new_text = "اللغة العربية"
-new_sequences = [char_to_int[char] for char in new_text]
-new_data = pad_sequences([new_sequences], maxlen=letters.shape[1])
-predictions = model.predict(new_data)
-print(''.join(int_to_char[i] for i in np.argmax(predictions, axis=1)))
+new_sequences = [cons_to_int[char] for char in new_text]
+new_data = to_categorical([new_sequences], num_classes=num_classes)
+predictions = model.predict(new_data.reshape((-1, 1, num_classes)))
+predicted_vowels = [int_to_vowel[i] for i in np.argmax(predictions, axis=1)]
+
+# Print the predicted diacritized phrase
+diacritized_phrase = ''
+for char, mark in zip(new_text, predicted_vowels):
+    diacritized_phrase += char + mark
+print(diacritized_phrase)
